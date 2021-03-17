7d0d59a2d7c2d7986c55326457c6cee79a6375ac
Antoine BERNARD <antoine_bernard@telecom-sudparis.eu>
2021-03-17 14:21:31 +0000

    pushes weights

--- Python/parameters.h
+++ Python/parameters.h
@@ -11,14 +11,14 @@
 
 const int hunit = HUNIT;
 
-const float lstm_cell_input_weights[4 * HUNIT] = {-0.47074094414711, 0.5782343149185181, 0.7150036096572876, 0.8580639362335205};
+const float lstm_cell_input_weights[4 * HUNIT] = {0.7236478924751282, -0.7236429452896118, -0.011296585202217102, 0.9588750004768372};
 
-const float lstm_cell_hidden_weights[4 * HUNIT * HUNIT] = {0.31322693824768066, -0.032405853271484375, -0.9226136207580566, 0.2227618396282196};
+const float lstm_cell_hidden_weights[4 * HUNIT * HUNIT] = {0.7760595083236694, -0.22524213790893555, 0.27519720792770386, -0.5208302736282349};
 
-const float lstm_cell_bias[4 * HUNIT] = {0.3858514428138733, 1.0, 0.3026246428489685, 0.3853971064090729};
+const float lstm_cell_bias[4 * HUNIT] = {0.2248043715953827, 1.0, -0.27169960737228394, 0.2257799506187439};
 
-float lstm_cell_hidden_layer[HUNIT] = {0.3922583758831024};
-float lstm_cell_cell_states[HUNIT] = {0.6355526447296143};
+float lstm_cell_hidden_layer[HUNIT] = {-0.40004485845565796};
+float lstm_cell_cell_states[HUNIT] = {-0.6508712768554688};
 
-const float dense_weights[HUNIT] = {0.7915790677070618};
-const float dense_bias = 0.25034016370773315;
+const float dense_weights[HUNIT] = {-1.345304012298584};
+const float dense_bias = 0.2549245059490204;
